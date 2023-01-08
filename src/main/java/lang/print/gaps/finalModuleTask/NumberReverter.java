0ac781385951485b55eaa0aef68cda315a6890c5
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-08 11:42:56 +0300

    corrected solution

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,11 +1,6 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public static void main(String[] args) {
-        NumberReverter nr = new NumberReverter();
-        nr.revert(536);
-
-    }
     public void revert(int number) {
         int reversedNumber = 0;
         while (number != 0 ){
